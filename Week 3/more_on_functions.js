17b476db6d261d6157c5b8798cde7533c3c746d0
Sri Darsan Sah <sridarsansah5907@gmail.com>
2025-02-09 22:10:34 +0530

    bit extra

--- Week 3/more_on_functions.js
+++ Week 3/more_on_functions.js
@@ -12,8 +12,12 @@ function test(a, b) {
 // JS is a both compiled and interpreted
 
 
+// asynchronus programming in js uses passing of function expression as callback
+// pub-sub architecture uses returning a function from a function
+
 // types of function expression
 // named, anonymous and IIFE (immediately invoked function expression)
+
 const f = function process() {
 
 }
